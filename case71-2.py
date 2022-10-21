469e67bd1588096027a9564b158f0fd1936e0bc9
Nikolas Lavrentiev <116113002+NikolasLav@users.noreply.github.com>
2022-10-21 14:01:20 +0000

    добавил функцию по подготовке списка покупок

--- case71-2.py
+++ case71-2.py
@@ -2,6 +2,23 @@ import os
 from pprint import pprint
 import unicodedata  #пришлось добавить, иначе некоторые пробелы и символы '|' воспринимались некорректно
 
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for item in cook_book[dish]:
+                ing, val, ms = item.values()
+                val = val * person_count 
+                if ing in shop_list:
+                    shop_list[ing]['quantity'] += val
+                else:
+                    shop_list[ing] = {'measure': ms, 'quantity': val}
+        else:
+            print(f'Блюда "{dish}" нет в кулинарной книге')
+    return pprint(shop_list)
+
+
 with open('recipes.txt', 'r', encoding='utf-8') as file:
     cook_book = {}
     for line in file:
@@ -16,5 +33,17 @@ with open('recipes.txt', 'r', encoding='utf-8') as file:
                 'quantity': int(s[1]),
                 'measure': s[2]
             })
+pprint(cook_book)
 
-print(cook_book)
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
+
+
+
+
+
+
+
+
+
+
+
